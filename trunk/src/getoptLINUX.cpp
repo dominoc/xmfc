a645c2424e1cc6195a77bbde8c7c633a44ba92b4
adelsberger@gmail.com <adelsberger@gmail.com@2b0cb36c-380a-66ac-31a2-89ad885e1528>
2011-04-01 09:11:16 +0000

    binary mode

--- trunk/src/getoptLINUX.cpp
+++ trunk/src/getoptLINUX.cpp
@@ -3,7 +3,7 @@
 char* optarg;
 int optind, opterr, optopt;
 
-int getopt(int argc, char *argv[], char *in_optstring, int* in_optind, char** out_optarg) {
+int getopt(int argc, char *argv[], const char *in_optstring, int* in_optind, char** out_optarg) {
 
   int c = getopt(argc,argv,in_optstring);
   *out_optarg = optarg;
